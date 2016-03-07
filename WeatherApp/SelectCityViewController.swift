aafcb84063ee1187b7cd26e89afdcc8084fcb72d
Arijan Ljoki <arijan_ljoki@hotmail.com>
2016-03-07 19:53:12 +0100

    Parse text with spaces, disable add button if no text

--- WeatherApp/SelectCityViewController.swift
+++ WeatherApp/SelectCityViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SelectCityViewController: UIViewController {
+class SelectCityViewController: UIViewController, UITextFieldDelegate {
     
     let apiKey = "546be23921ef651bb1c511c2e6477f79"
     @IBOutlet weak var selectCityTextField: UITextField!
@@ -17,15 +17,24 @@ class SelectCityViewController: UIViewController {
     
     override func viewDidLoad() {
         let addButton = UIBarButtonItem(barButtonSystemItem: .Save, target: self, action: "saveNewCity:")
-        self.navigationItem.rightBarButtonItem = addButton
+        navigationItem.rightBarButtonItem = addButton
+        navigationItem.rightBarButtonItem?.enabled = false
+        
+        selectCityTextField.addTarget(self, action: "textFieldDidChange:", forControlEvents: UIControlEvents.EditingChanged)
+    }
+    
+    func textFieldDidChange(textField: UITextField) {
+        let inputText = textField.text!.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        navigationItem.rightBarButtonItem?.enabled = inputText.characters.count > 0 ? true : false
     }
     
     func saveNewCity(sender: AnyObject) {
-        let cityName = selectCityTextField.text!
+        let cityName = selectCityTextField.text!.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())!
+        
         var city: City!
         let url = "http://api.openweathermap.org/data/2.5/weather?q=\(cityName)&APPID=\(apiKey)&units=metric"
         
-        let task = NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: url)!, completionHandler: { (data, response, error) -> Void in
+        let task = NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: url)!, completionHandler: { [weak self] (data, response, error) -> Void in
             do{
                 let jsonResponse = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! [String:AnyObject]
                 
@@ -36,19 +45,17 @@ class SelectCityViewController: UIViewController {
                 let description = jsonResponse["weather"]!.objectAtIndex(0)["description"]
                 
                 city = City.init(id: id, name: name as! String, temperature: temperature, humidity: humidity, description: description as! String)
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    
+                    //        Block returning selected city
+                    self?.onCitySelected(city)
+                    self?.navigationController?.popViewControllerAnimated(true)
+                })
             }
             catch {
                 print("json error: \(error)")
             }
         })
         task.resume()
-        
-//        returnSelectedCity(city)
-    }
-    
-    private func returnSelectedCity(city: City) {
-//        Block returning selected city
-        onCitySelected(city)
-        navigationController?.popViewControllerAnimated(true)
     }
 }
